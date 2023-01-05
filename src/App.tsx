a6158ff03d4a695936a7275ef96f60de63614c13
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2023-01-05 11:57:46 +0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [gender, setGender] = useState('2');
+  const [rules, setRules] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={() => setName('ahmet')}>Adı Değştir</button>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} /><br/>
+
+      <textarea value={description} onChange={e=> setDescription(e.target.value)}/> <br />
+      
+      <select value={gender} onChange={e => setGender(e.target.value)}>
+        <option value="">Seçin</option>
+        <option value="1">Erkek</option>
+        <option value="2">Kadın</option>
+      </select> <br/>
+
+      <label>
+        <input type="checkbox" checked={rules} onChange={e => setRules(e.target.checked)} />
+        Kuralları Kabul Ediyorum !
+      </label>
+      <br/>
+      <button disabled={!rules}>Devam Et</button>
+    </>
   );
 }
 
